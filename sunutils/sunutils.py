71b52c71f9d1fb1d2910ddf981f6c3d0d0f70de8
Manjiro-Sano <93420172+Manjiro-Sano@users.noreply.github.com>
2022-04-30 20:26:02 +0530

    Update sunutils.py

--- sunutils/sunutils.py
+++ sunutils/sunutils.py
@@ -5,7 +5,6 @@ from pyrogram.utils import MAX_CHANNEL_ID
 from pytgcalls import PyTgCalls
 import config
 import asyncio
-from yt_dlp import YoutubeDL
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 group_call = PyTgCalls(client)
@@ -13,16 +12,3 @@ quu = {}
 block_chat = []
 GROUP_CALL = {}
 FFMPEG_PRO = {}
-
-async def yt_download(ytlink):
-    ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
-        'outtmpl': '%(id)s-%(extractor)s.%(ext)s',
-        'writethumbnail': False
-    }
-    with YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(ytlink, download=False)
-        ydl.process_info(info_dict)
-        file = ydl.prepare_filename(info_dict)
-        tit = info_dict.get('title')
-        return file, tit
